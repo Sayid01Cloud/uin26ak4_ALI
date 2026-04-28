656b5ae4a4e4a907201eedd0781ec9eee81448c4
Sayid01Cloud <sidali2029@gmail.com>
2026-04-28 17:50:12 +0200

    Flyttet backend inn i hovedrepo

---
+++ backend/schemaTypes/Arbeidskrav.js
@@ -0,0 +1,17 @@
+export default {
+    name: "Arbeidskrav",
+    title: "Arbeidskrav",
+    type: "document",
+    fields: [
+        {
+            name: "tittel",
+            title: "Tittel",
+            type: "string"
+        },
+        {
+            name: "beskrivelse",
+            title: "Beskrivelse",
+            type: "text"
+        }
+    ]
+}
---
+++ backend/schemaTypes/index.js
@@ -0,0 +1,4 @@
+import Arbeidskrav from "./Arbeidskrav";
+
+export const schemaTypes = [Arbeidskrav]
+
